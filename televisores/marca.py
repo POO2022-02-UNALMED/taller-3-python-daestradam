f0164ec301ba72efbd43d606b7b555958d09dbd4
Daniel Estrada <daestradam@unal.edu.co>
2022-08-29 15:16:34 -0500

    clases tv

---
+++ televisores/marca.py
@@ -0,0 +1,9 @@
+class Marca:
+    def __init__(self, nombre):
+        self._nombre = nombre
+    
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getNombre(self, nombre):
+        return self._nombre
